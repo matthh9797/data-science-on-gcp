6d04eed6d6265c465905faeee7be1fabd58e5177
matthh9797 <matthewh@netextremity.com>
2022-01-29 17:14:10 +0000

    Uncommented out code

--- 02_ingest/zip_to_csv.sh
+++ 02_ingest/zip_to_csv.sh
@@ -3,6 +3,6 @@ echo ${YEAR:=2021}  # default if YEAR not set
 for month in `seq -w 1 12`; do 
    unzip $YEAR$month.zip
    # Unzipped file pattern {random_numbers}_ONTIME_REPORTING.csv
-   # mv *ONTIME_REPORTING.csv $YEAR$month.csv
+   mv *ONTIME_REPORTING.csv $YEAR$month.csv
    rm $YEAR$month.zip
 done
